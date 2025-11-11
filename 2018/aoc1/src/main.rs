3004a513c08c9b908392c5c271b7a536dbf7860a
Daniel Fox <foxdanie@gmail.com>
2025-11-11 10:32:33 -0500

    solution to part 2

--- 2018/aoc1/src/main.rs
+++ 2018/aoc1/src/main.rs
@@ -1,10 +1,14 @@
+use std::collections::HashSet;
 use std::fs::{File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
 
 fn main() -> std::io::Result<()> {
-    let file = OpenOptions::new().read(true).open("input/input.txt")?;
-    let buf = BufReader::new(file);
-    part1(buf)?;
+    let file1 = OpenOptions::new().read(true).open("input/input.txt")?;
+    let file2 = OpenOptions::new().read(true).open("input/input.txt")?;
+    let buf1 = BufReader::new(file1);
+    let buf2 = BufReader::new(file2);
+    part1(buf1)?;
+    part2(buf2)?;
     Ok(())
 }
 
@@ -18,4 +22,20 @@ fn part1(buf: BufReader<File>) -> std::io::Result<()> {
     Ok(())
 }
 
-// fn part2(file: File) -> std::io::Result<()> {}
+fn part2(buf: BufReader<File>) -> std::io::Result<()> {
+    let mut hs = HashSet::<i32>::new();
+    let mut freq = 0i32;
+    hs.insert(freq);
+    let vec: Vec<i32> = buf
+        .lines()
+        .map(|item| item.unwrap().replace("+", "").parse::<i32>().unwrap())
+        .collect();
+    for item in vec.iter().cycle() {
+        freq += item;
+        if !hs.insert(freq) {
+            println!("{freq}");
+            break;
+        }
+    }
+    Ok(())
+}
