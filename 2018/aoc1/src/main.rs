2fae135cc819b882c7a5d39407e595c5276e528e
Daniel Fox <foxdanie@gmail.com>
2025-11-11 09:58:46 -0500

    add solution to aoc1 part1

--- 2018/aoc1/src/main.rs
+++ 2018/aoc1/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::{File, OpenOptions};
+use std::io::{BufRead, BufReader, Write};
+
 fn main() -> std::io::Result<()> {
+    let file = OpenOptions::new().read(true).open("input/input.txt")?;
+    let buf = BufReader::new(file);
+    part1(buf)?;
     Ok(())
 }
+
+fn part1(buf: BufReader<File>) -> std::io::Result<()> {
+    let freq = buf.lines().fold(0, |acc, s| {
+        acc + s.unwrap().replace("+", "").parse::<i32>().unwrap()
+    });
+    let freq = format!("{freq}\n");
+    let freq = freq.as_bytes();
+    std::io::stdout().write_all(freq)?;
+    Ok(())
+}
+
+// fn part2(file: File) -> std::io::Result<()> {}
