b7a4fb000ddeed864c6b1f80bd618b5af018d0b9
Daniel Fox <foxdanie@gmail.com>
2025-11-16 21:38:23 -0500

    add day 3

---
+++ 2018/aoc3/main.rs
@@ -0,0 +1,19 @@
+use std::fs;
+use std::io::Write;
+
+fn main() -> std::io::Result<()> {
+    let input = fs::read_to_string(input/input.txt)?;
+
+    part1(&input)?;
+    part2(&input)?;
+
+    Ok(())
+}
+
+fn part1(input: &str) -> std::io::Result<()> {
+    Ok(())
+}
+
+fn part2(input: &str) -> std::io::Result<()> {
+    Ok(())
+}
