cc4fac1e57d4cdb49b9fd4c9c1729f25f9e289f6
Daniel Fox <foxdanie@gmail.com>
2025-11-18 10:51:31 -0500

    only print first few values

--- 2018/aoc3/src/main.rs
+++ 2018/aoc3/src/main.rs
@@ -57,7 +57,7 @@ fn part1(input: &str) -> Result<(), Box<dyn std::error::Error>> {
             .unwrap();
         claims.push(claim);
     }
-    writeln!(io::stdout(), "{:#?}", claims)?;
+    writeln!(io::stdout(), "{:#?}", claims[..3])?;
     Ok(())
 }
 
