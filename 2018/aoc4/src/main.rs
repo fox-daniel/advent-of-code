908534e21e395e4bed54da64db44a48ac4961201
Daniel Fox <foxdanie@gmail.com>
2025-12-01 21:32:37 -0500

    move sorting into function and test

--- 2018/aoc4/src/main.rs
+++ 2018/aoc4/src/main.rs
@@ -62,8 +62,7 @@ fn part1(input: &str) -> Result<(), Box<dyn std::error::Error>> {
         records.push(line.parse()?);
     }
 
-    records.sort_by_key(|r| r.datetime);
-    // println!("{:#?}", &records[..3]);
+    sort_records(&mut records);
 
     let mut sleep_counts = HashMap::<u32, u32>::new();
     let mut current_id = records[0].id.unwrap();
@@ -78,7 +77,7 @@ fn part1(input: &str) -> Result<(), Box<dyn std::error::Error>> {
                 .or_insert(1);
         }
     }
-    // println!("{:#?}", sleep_counts);
+    println!("{:#?}", sleep_counts);
     let (id, _) = sleep_counts.iter().fold((u32::MAX, 0), |acc, item| {
         if *item.1 > acc.1 {
             (*item.0, *item.1)
@@ -112,6 +111,10 @@ fn part1(input: &str) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn sort_records(records: &mut [Record]) {
+    records.sort_by_key(|r| r.datetime);
+}
+
 fn part2(input: &str) -> std::io::Result<()> {
     Ok(())
 }
@@ -119,6 +122,7 @@ fn part2(input: &str) -> std::io::Result<()> {
 #[cfg(test)]
 mod test {
     use super::*;
+    use chrono::{NaiveDate, NaiveTime};
     const INPUT_LINES: &str = "[1518-04-16 00:46] falls asleep
     [1518-10-31 00:46] wakes up
     [1518-10-14 00:02] Guard #2459 begins shift";
@@ -143,6 +147,36 @@ mod test {
         let line = INPUT_LINES.lines().nth(1).unwrap();
         let record: Record = line.parse()?;
         assert!(!record.sleep.unwrap());
+        Ok(())
+    }
+
+    #[test]
+    fn sort_records_test() -> Result<(), Box<dyn std::error::Error + 'static>> {
+        let mut records = Vec::<Record>::new();
+        for line in INPUT_LINES.lines() {
+            records.push(line.parse()?)
+        }
+        sort_records(&mut records);
+        assert_eq!(
+            records[0].datetime,
+            DateTime::<Utc>::from_naive_utc_and_offset(
+                NaiveDateTime::new(
+                    NaiveDate::from_ymd_opt(1518, 4, 16).expect("valid date"),
+                    NaiveTime::from_hms_opt(0, 46, 0).expect("valide hour")
+                ),
+                Utc
+            )
+        );
+        assert_eq!(
+            records[1].datetime,
+            DateTime::<Utc>::from_naive_utc_and_offset(
+                NaiveDateTime::new(
+                    NaiveDate::from_ymd_opt(1518, 10, 14).expect("valid date"),
+                    NaiveTime::from_hms_opt(0, 2, 0).expect("valid time"),
+                ),
+                Utc
+            )
+        );
         Ok(())
     }
 }
