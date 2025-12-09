2fd783b03d25705995436ea584c1481ebed16bce
Daniel Fox <foxdanie@gmail.com>
2025-12-09 15:43:53 -0500

    use bytes instead of chars

--- 2018/aoc5/src/main.rs
+++ 2018/aoc5/src/main.rs
@@ -6,9 +6,9 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("input/input.txt")?;
-
-    part1(&input)?;
-    part2(&input)?;
+    let input = input.trim();
+    part1(input)?;
+    part2(input)?;
 
     Ok(())
 }
@@ -18,16 +18,13 @@ fn part1(input: &str) -> Result<()> {
     Ok(())
 }
 
-fn part1_result(s: &str) -> Vec<char> {
-    let mut protein: Vec<char> = s.chars().collect();
-    if protein.last().unwrap() == &'\n' {
-        protein.pop();
-    }
+fn part1_result(s: &str) -> Vec<u8> {
+    let mut protein: Vec<u8> = s.as_bytes().to_vec();
     react(&mut protein)
 }
 
-fn react(protein: &mut [char]) -> Vec<char> {
-    let mut prefix: Vec<char> = vec![];
+fn react(protein: &mut [u8]) -> Vec<u8> {
+    let mut prefix: Vec<u8> = vec![];
     let len = protein.len();
     let mut idx = 0;
     while idx < len {
@@ -53,17 +50,20 @@ fn react(protein: &mut [char]) -> Vec<char> {
     prefix.into_iter().collect()
 }
 
-fn annihilate(c1: char, c2: char) -> bool {
-    ((c1.is_lowercase() & c2.is_uppercase()) | (c2.is_lowercase() & c1.is_uppercase()))
-        & (c1.to_lowercase().next() == c2.to_lowercase().next())
+fn annihilate(c1: u8, c2: u8) -> bool {
+    if c1 > c2 {
+        c1 - c2 == 32
+    } else {
+        c2 - c1 == 32
+    }
 }
 
 fn part2(input: &str) -> Result<()> {
     let result = part1_result(input);
     let mut min_len = u32::MAX;
-    for c in 'a'..='z' {
+    for m in 'a'..='z' {
         let mut clean = result.clone();
-        clean = remove_impurities(clean, c);
+        clean = remove_impurities(clean, m as u8);
         clean = react(&mut clean);
         min_len = min(min_len, clean.len() as u32)
     }
@@ -71,10 +71,13 @@ fn part2(input: &str) -> Result<()> {
     Ok(())
 }
 
-fn remove_impurities(clean: Vec<char>, m: char) -> Vec<char> {
+fn remove_impurities(clean: Vec<u8>, m: u8) -> Vec<u8> {
     clean
         .into_iter()
-        .filter(|c| c.to_lowercase().next() != Some(m))
+        .filter(|c| {
+            let c = if c > &96u8 { c } else { &(*c + 32u8) };
+            c != &m
+        })
         .collect()
 }
 
@@ -83,15 +86,15 @@ mod test {
     use super::*;
     #[test]
     fn annihilate_test1() {
-        assert!(annihilate('A', 'a'));
+        assert!(annihilate(b'A', b'a'));
     }
     #[test]
     fn annihilate_test2() {
-        assert!(!annihilate('A', 'A'));
+        assert!(!annihilate(b'A', b'A'));
     }
     #[test]
     fn annihilate_test3() {
-        assert!(!annihilate('A', 'b'));
+        assert!(!annihilate(b'A', b'b'));
     }
     #[test]
     fn part1_test1() {
@@ -111,16 +114,12 @@ mod test {
     fn part1_test3() {
         let input = "dabAcCaCBAcCcaDA";
         let result = part1_result(input);
-        let result: String = result.into_iter().collect();
-        println!("result={result:?}");
-        assert_eq!(&result, "dabCBAcaDA");
+        assert_eq!(&result, "dabCBAcaDA".as_bytes());
     }
     #[test]
     fn part1_test4() {
         let input = "abcdeEDCfghIiHGkl";
         let result = part1_result(input);
-        let result: String = result.into_iter().collect();
-        println!("result={result:?}");
-        assert_eq!(&result, "abfkl");
+        assert_eq!(&result, "abfkl".as_bytes());
     }
 }
